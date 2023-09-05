2a06cd4802a3f57f17a8d76892d4d01efd8888b2
insideyiu <a22311061@uthermosillo.edu.mx>
2023-09-04 19:59:04 -0700

    Implementar servicio

--- src/app/cajero/pages/pago-page/pago-page.component.ts
+++ src/app/cajero/pages/pago-page/pago-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CajeroService } from '../../services/cajero.service';
 
 @Component({
   selector: 'app-pago-page',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./pago-page.component.css']
 })
 export class PagoPageComponent {
+  public numbers: number[] = [1, 2, 3, 4 , 5, 6, 7, 8, 9];
+  public total: number[] = [];
 
+  constructor(
+    private cajeroService: CajeroService,
+    private router: Router){}
+
+  agregarNumero(numero: number): void {
+    this.total.push(numero);
+  }
+
+  borrarNumero(): void {
+    this.total.pop();
+  }
+
+  limpiarInput(): void {
+    this.total = [];
+  }
+
+  get valueInput(): string {
+    let cadena: string = '';
+      this.total.forEach( valor => {
+      cadena += valor;
+    })
+
+    return cadena;
+  }
+
+  validarPago(pago: string): void {
+    if(Number(pago) <= 0 ) return;
+
+    this.cajeroService.pago = Number(pago);
+    this.router.navigateByUrl('/cajero/cambio')
+  }
 }
